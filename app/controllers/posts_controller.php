dc53cd4ca8beec2a96650800e0de49e38e0b0ae8
Hirohide Sano @sanojimaru <sanojimaru@gmail.com>
2011-10-16 03:14:26 +0900

    Add cakephp-pjax-plugin demo.

--- app/controllers/posts_controller.php
+++ app/controllers/posts_controller.php
@@ -1,61 +1,62 @@
 <?php
 class PostsController extends AppController {
 
-	var $name = 'Posts';
+  public $components = array('Pjax.Pjax');
+  public $helpers = array('Pjax.Pjax');
 
-	function index() {
-		$this->Post->recursive = 0;
-		$this->set('posts', $this->paginate());
-	}
+  public function index() {
+    $this->Post->recursive = 0;
+    $this->set('posts', $this->paginate());
+  }
 
-	function view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid post', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('post', $this->Post->read(null, $id));
-	}
+  public function view($id = null) {
+    if (!$id) {
+      $this->Session->setFlash(__('Invalid post', true));
+      $this->Pjax->redirect(array('action' => 'index'));
+    }
+    $this->set('post', $this->Post->read(null, $id));
+  }
 
-	function add() {
-		if (!empty($this->data)) {
-			$this->Post->create();
-			if ($this->Post->save($this->data)) {
-				$this->Session->setFlash(__('The post has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The post could not be saved. Please, try again.', true));
-			}
-		}
-	}
+  public function add() {
+    if (!empty($this->data)) {
+      $this->Post->create();
+      if ($this->Post->save($this->data)) {
+        $this->Session->setFlash(__('The post has been saved', true));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Session->setFlash(__('The post could not be saved. Please, try again.', true));
+      }
+    }
+  }
 
-	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid post', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->Post->save($this->data)) {
-				$this->Session->setFlash(__('The post has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The post could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Post->read(null, $id);
-		}
-	}
+  public function edit($id = null) {
+    if (!$id && empty($this->data)) {
+      $this->Session->setFlash(__('Invalid post', true));
+      $this->redirect(array('action' => 'index'));
+    }
+    if (!empty($this->data)) {
+      if ($this->Post->save($this->data)) {
+        $this->Session->setFlash(__('The post has been saved', true));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Session->setFlash(__('The post could not be saved. Please, try again.', true));
+      }
+    }
+    if (empty($this->data)) {
+      $this->data = $this->Post->read(null, $id);
+    }
+  }
 
-	function delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for post', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->Post->delete($id)) {
-			$this->Session->setFlash(__('Post deleted', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(__('Post was not deleted', true));
-		$this->redirect(array('action' => 'index'));
-	}
+  public function delete($id = null) {
+    if (!$id) {
+      $this->Session->setFlash(__('Invalid id for post', true));
+      $this->redirect(array('action'=>'index'));
+    }
+    if ($this->Post->delete($id)) {
+      $this->Session->setFlash(__('Post deleted', true));
+      $this->redirect(array('action'=>'index'));
+    }
+    $this->Session->setFlash(__('Post was not deleted', true));
+    $this->redirect(array('action' => 'index'));
+  }
 }
